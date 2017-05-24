e0784d24e52925859720a76848715d2059314eba
agagrzyb <agigrzyb@gmial.com>
2017-05-24 09:36:57 +0100

    add files

--- ExerciseSolutions/src/sheet7EmployeeAndClockClasses/MainForEmployeeClass.java
+++ ExerciseSolutions/src/sheet7EmployeeAndClockClasses/MainForEmployeeClass.java
@@ -1,5 +1,39 @@
 package sheet7EmployeeAndClockClasses;
 
+
+
 public class MainForEmployeeClass {
+
+	public static void main(String[] args) {
+		EmployeeClass Employee1 = new EmployeeClass();
+		System.out.print("Employee number : " + 
+				EmployeeClass.employeeNumberCounter);
+		Employee1.setName("Mary");
+		Employee1.setAge(45);
+		Employee1.setSalary(45_000);
+		Employee1.setNoOfSickDays(6);
+		System.out.println(Employee1);
+	
+		System.out.println("----------------------");
+		EmployeeClass Employee2 = new EmployeeClass();
+		System.out.print("Employee number : " + 
+				EmployeeClass.employeeNumberCounter);
+		Employee2.setName("John");
+		Employee2.setAge(40);
+		Employee2.setSalary(48_000);
+		Employee2.setNoOfSickDays(8);
+		System.out.println(Employee2);
+
+		System.out.println("----------------------");
+		EmployeeClass Employee3 = new EmployeeClass();
+		System.out.print("Employee number : " + 
+				EmployeeClass.employeeNumberCounter);
+		Employee3.setName("Nick");
+		Employee3.setAge(28);
+		Employee3.setSalary(28_000);
+		Employee3.setNoOfSickDays(12);
+		System.out.println(Employee3);
+	}
+
 
 }
