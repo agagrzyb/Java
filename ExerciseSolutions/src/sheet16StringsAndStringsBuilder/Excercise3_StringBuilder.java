b10f9e900382fe27793c61435687e04c7716e812
agagrzyb <agigrzyb@gmial.com>
2017-06-12 09:43:37 +0100

    add files

---
+++ ExerciseSolutions/src/sheet16StringsAndStringsBuilder/Excercise3_StringBuilder.java
@@ -0,0 +1,10 @@
+package sheet16StringsAndStringsBuilder;
+
+public class Excercise3_StringBuilder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- ExerciseSolutions/src/sheet16StringsAndStringsBuilder/Exercise1_ComparingStrings.java
+++ ExerciseSolutions/src/sheet16StringsAndStringsBuilder/Exercise1_ComparingStrings.java
@@ -11,18 +11,18 @@ public class Exercise1_ComparingStrings {
 
 
 		//strA and strB do not refer to the same object
-		System.out.println("Monday and Monday do not refer to the same object: strA == strB : " + (strA == strB));
+		System.out.println("Monday and Monday do refer to the same object: strA == strB : " + (strA == strB));
 		//strC and strD do refer to the same object 
 		System.out.println("Tuesday and Tuesday do refer to the same object : strC == strD : " + (strC == strD));
 		// using .equals() show that strA and strB contain the same letters AND strC and strD contain the same letters
-		System.out.println();
+		System.out.println("Monday and Monday contain the same letters AND Tuesday and Tuesday contain the same letters : strA .equals strB && strC .equals strD : " + ((strA.equals(strB)) && (strC.equals(strD))));
 		//strD and strE do not refer to the same object
-		System.out.println("Tuesday and Tuesday do not refer to the same object : strE == strD : " + (strE == strD));
+		System.out.println("Tuesday and Tuesday do refer to the same object : strE == strD : " + (strE == strD));
 		// using toUpperCase() make strC refer to "Tuesday", now check if strC and strD still refer to the same object
-		System.out.println();
+		strC = strC.toUpperCase();
+		System.out.println("strC toUpperCase : " + strC);
+		System.out.println("strC and strD still refer to the same object : strC == strD : " + (strC == strD));	
 		// using .equals() test if strC and strD contain the same letters
-		System.out.println();
-
+		System.out.println("strC and strD contain the same letters : " + (strC.equals(strD)));
 	}
-
 }
