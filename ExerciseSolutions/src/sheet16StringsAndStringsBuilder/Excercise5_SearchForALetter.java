6b450bcdc56d1974f5c7a7b5b48d9adbda7dadd9
agagrzyb <agigrzyb@gmial.com>
2017-06-12 12:56:42 +0100

    add files

---
+++ ExerciseSolutions/src/sheet16StringsAndStringsBuilder/Excercise5_SearchForALetter.java
@@ -0,0 +1,10 @@
+package sheet16StringsAndStringsBuilder;
+
+public class Excercise5_SearchForALetter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- ExerciseSolutions/src/sheet16StringsAndStringsBuilder/Exercise4_Reverse.java
+++ ExerciseSolutions/src/sheet16StringsAndStringsBuilder/Exercise4_Reverse.java
@@ -1,5 +1,36 @@
 package sheet16StringsAndStringsBuilder;
 
-public class Exercise4_Reverse {
+import java.util.Scanner;
 
+public class Exercise4_Reverse {
+	public static void main(String[]args) {
+
+		System.out.println("Have your say...");
+		Scanner sc = new Scanner(System.in);
+		String input = sc.nextLine();		
+		System.out.println("Your say : " + input);
+		sc.close();
+		//Palindrome...
+		//converts the string to an array of characters (first for loop),
+		//reverses the array into a second array (second for loop),
+		//and then converts back to a string.
+		int len = input.length();
+		char [] tempCharArray = new char[len];
+		char [] charArray = new char[len];
+		//put original string in an array of chars
+		for(int i=0; i<len; i++){
+			tempCharArray[i] = input.charAt(i);
+		}
+		// reverse array of chars
+		for(int j = 0; j < len; j++){
+			charArray[j] = tempCharArray[len -1 -j];
+		}
+		String reverse = new String(charArray);
+		System.out.println("Your say reversed : " + reverse);
+
+		//convert the palindrome string to a string builder, using the reverse() method in the StringBuilder class.
+		StringBuilder sb = new StringBuilder(input);
+		sb.reverse();  // reverse it
+		System.out.println("StringBuilder : " + sb);
+	}
 }
