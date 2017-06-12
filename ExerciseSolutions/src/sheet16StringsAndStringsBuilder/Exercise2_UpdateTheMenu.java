f92753fa9b442b3d50745e5565530a66d15e2b60
agagrzyb <agigrzyb@gmial.com>
2017-06-12 10:50:57 +0100

    add files

--- ExerciseSolutions/src/sheet16StringsAndStringsBuilder/Exercise2_UpdateTheMenu.java
+++ ExerciseSolutions/src/sheet16StringsAndStringsBuilder/Exercise2_UpdateTheMenu.java
@@ -1,5 +1,22 @@
 package sheet16StringsAndStringsBuilder;
 
 public class Exercise2_UpdateTheMenu {
+	public static void main (String [] args){
 
+		String todayMenu = "Wednesday's Menu: Roast turkey with homemade turkey gravy, potatoes and veg of the day.";
+		System.out.println(todayMenu);
+		//change "Wednesday" to "Thursday"
+		todayMenu = todayMenu.replace("Wednesday", "Thursday");
+		System.out.println(todayMenu);
+		//change "turkey" to "beef"
+		todayMenu = todayMenu.replace("turkey", "beef");
+		System.out.println(todayMenu);
+		//add "Jelly and ice cream for dessert"
+		todayMenu = todayMenu.concat(" Jelly and ice cream for dessert.");
+		System.out.println(todayMenu);
+		//remove "and ice cream"
+		todayMenu = todayMenu.replaceAll(" and ice cream ", " ");
+		System.out.println(todayMenu);
+
+	}
 }
