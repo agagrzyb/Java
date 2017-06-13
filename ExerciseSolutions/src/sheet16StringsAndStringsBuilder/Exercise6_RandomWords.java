f933eef27d8318da7cf7abc85f96eaeae6214866
agagrzyb <agigrzyb@gmial.com>
2017-06-13 15:45:36 +0100

    add files

--- ExerciseSolutions/src/sheet16StringsAndStringsBuilder/Exercise6_RandomWords.java
+++ ExerciseSolutions/src/sheet16StringsAndStringsBuilder/Exercise6_RandomWords.java
@@ -3,6 +3,10 @@ package sheet16StringsAndStringsBuilder;
 public class Exercise6_RandomWords {
 
 	public static void main(String[] args) {
+		System.out.println("-- Random Words --");
+		int[] myArray = {0,1,2,3,4};
+		int randomNumber = (int)(Math.random()*5);
+		System.out.println(randomNumber);
 		
 	}
 
