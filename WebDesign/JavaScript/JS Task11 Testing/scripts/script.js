129015718a8393456d96b48c6d5b8f7a10edd30e
agagrzyb <agigrzyb@gmial.com>
2017-07-19 13:17:39 +0100

    add files

--- WebDesign/JavaScript/JS Task11 Testing/scripts/script.js
+++ WebDesign/JavaScript/JS Task11 Testing/scripts/script.js
@@ -3,30 +3,23 @@
 console.log('script.js file loaded');
 
 // step9. create a function
-var divTag = document.getElementById('secondDiv');
 
 document.addEventListener("DOMContentLoaded", function (event) {
       console.log("DOM fully loaded and parsed");
-
+      var divTag = document.getElementById('secondDiv');
       // Unobstrusive events with addEventListener
       document.getElementById("h1").addEventListener("click", toggleDisplay);
+
+      // function with if/else statement that toggles the 'secondDiv' ON and OFF when the h1 element is clicked
+      function toggleDisplay() {
+            console.log('inside toggleDiplay function');
+            if (divTag.style.display == 'block' || divTag.style.display == '') {
+                  divTag.style.display = 'none';
+            }
+            else {
+                  divTag.style.display = 'block';
+            }
+      }
+
 });
-// function with if/else statement that toggles the 'secondDiv' ON and OFF when the h1 element is clicked
-function toggleDisplay() {
-      console.log('inside toggleDiplay function');
-      if (divTag.style.display == '') {
-            divTag.style.display = 'none';
-      }
-      else {
-            divTag.style.display = '';
-      }
-}
-
-// important
-/*
-clicking the 'secondDiv' element will invoke the function 
-*/
-
-
-
 
