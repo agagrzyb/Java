4377642c3a3aefae6603a421cdf8b02f5e53cb03
agagrzyb <agigrzyb@gmial.com>
2017-08-03 15:30:58 +0100

    add files

---
+++ OCA_Revision/src/Chapter_1/GarbageCollectionOne.java
@@ -0,0 +1,10 @@
+package Chapter_1;
+
+public class GarbageCollectionOne {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ OCA_Revision/src/Chapter_1/OrderOfInitBlocks.java
@@ -0,0 +1,10 @@
+package Chapter_1;
+
+public class OrderOfInitBlocks {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
